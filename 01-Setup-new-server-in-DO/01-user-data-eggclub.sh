9d8d1242e7aea6b0b4f4598b3b5e9fb2e524ae60
Cuong Tran <tranhuucuong91@gmail.com>
2016-12-18 01:54:23 +0700

    Update user-data.sh: create default zshrc

--- 01-Setup-new-server-in-DO/01-user-data-eggclub.sh
+++ 01-Setup-new-server-in-DO/01-user-data-eggclub.sh
@@ -13,10 +13,14 @@ sudo apt-get install -y htop monit
 USERS="prod"
 
 echo "Adding users:"
-# Add ssh-key
 for user in $USERS; do
     echo "- Add ${user}"
     adduser --disabled-password --gecos "" ${user}
+
+    # Create default zshrc
+    touch /home/${user}/.zshrc
+
+    # Create default SSH authorized_keys
     mkdir /home/${user}/.ssh
     touch /home/${user}/.ssh/authorized_keys
 
@@ -24,13 +28,13 @@ for user in $USERS; do
     chmod 755 /home/${user}/.ssh
     chmod 644 /home/${user}/.ssh/authorized_keys
 
-    # add cuongtran ssh key
+    # Add cuongtran ssh key
     cat >> /home/${user}/.ssh/authorized_keys<<'EOM'
 # Cuong Tran
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDACHkvrU00GAAKvyZkmxaNWyp+DjN8YmK6qr38MBPQSzfPVR/AsIRhB7Vc/+yB3XnbnGIsib175CmjC3jtsdm1yZbXrGpBpE0BEArwQirLT9eLVC73C3SFkSZ2RpCIQbHD7EV8SYg6cYlk3SivtrjZdl9kbObstlvt68yr998oEdydA5K8Wql9xUHJPN6x8yYgGk33TGnlSr6qY32vcEpvwruRx9SKozHj+q45hwLNVVjtcH9cecMV+tVF3uXgoAl6f1j/QJZtmycsimmomh909AywlHNmsUuBd2VLt1M4qtWQTC21NSiBDduDCMRe6aQHShULdCuOAHHUILWqqSv5 tranhuucuong91@gmail.com
 EOM
 
-    # grant sudo
+    # Grant sudo
     cat >> /etc/sudoers<<EOM
 ${user}    ALL=(ALL:ALL) NOPASSWD:ALL
 EOM
